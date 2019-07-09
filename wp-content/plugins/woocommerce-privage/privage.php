decffd9f0912896749e35042c4924463f480c00a
Smith Krengkrud <smith.kre@gmail.com>
2019-07-09 19:42:40 +0700

    integrate hook login

--- wp-content/plugins/woocommerce-privage/privage.php
+++ wp-content/plugins/woocommerce-privage/privage.php
@@ -27,7 +27,6 @@ function private_coupon_checking() {
 // Override coupon ajax
 add_action('wc_ajax_apply_coupon', 'private_coupon_checking');
 
-
 function privage_locate_template( $template, $template_name, $template_path ) {
   $basename = basename( $template );
   if( $basename == 'form-login.php' ) {
@@ -37,3 +36,77 @@ function privage_locate_template( $template, $template_name, $template_path ) {
 }
 
 add_filter( 'woocommerce_locate_template', 'privage_locate_template', 10, 3 );
+
+// Apply script
+add_action('wc_ajax_login_callback', 'privage_login' );
+
+function privage_login() {
+  // Get access_token
+  try {
+    $code = $_GET['code'];
+    $url = "https://account.privageapp.com/grant?code=".$code;
+
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+    $output = curl_exec($ch); 
+    $result = json_decode($output);
+
+    curl_close($ch); 
+
+    $access_token = $result->access_token;
+
+    // Get profile
+    $url = "https://account.privageapp.com/user/profile";
+
+    $ch = curl_init(); 
+    curl_setopt($ch, CURLOPT_URL, $url); 
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+      'Authorization: Bearer '.$access_token
+    ));
+
+    $output = curl_exec($ch); 
+    $user_profile = json_decode($output);
+
+    curl_close($ch);
+
+    $user_id = $user_profile->user_id;
+    $name = $user_profile->profile->full_name;
+    $email = $user_profile->profile->email;
+    $identifier = $user_profile->identifier;
+
+    if (!username_exists( $user_id )) {
+      wp_create_user( $user_id, $user_id.$identifier, $email );
+    }
+
+    $creds = array();
+    $creds['user_login'] = $user_id;
+    $creds['user_password'] = $user_id.$identifier;
+    $creds['remember'] = true;
+    wp_signon($creds, false);
+
+    // Update profile
+    $current_user = get_user_by('login', $user_id);
+
+    $result = wp_update_user(array(
+      'ID' => $current_user->ID,
+      'nickname' => $name,
+      'display_name' => $name
+    ));
+    
+  } catch (Exception $e) { 
+    echo $e;
+  }
+
+  ?>
+  <script>
+  window.onload = function() {
+    window.opener.location.reload();
+    window.close();
+  }
+  </script>
+  <?php
+  exit();
+}
