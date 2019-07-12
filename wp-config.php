45a54977590e171fd5bf93187798549ecb073482
gee <jeerawut.te@gmail.com>
2019-07-12 17:20:47 +0700

    aaa

--- wp-config.php
+++ wp-config.php
@@ -23,10 +23,10 @@
 define('DB_NAME', 'kmkmonline');
 
 /** MySQL database username */
-define('DB_USER', 'server_test');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'server_test');
+define('DB_PASSWORD', '');
 
 /** MySQL hostname */
 define('DB_HOST', '127.0.0.1');
