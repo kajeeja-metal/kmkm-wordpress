a287ecf138f17b8370998d5bf683bb3b66a80ed5
gee <jeerawut.te@gmail.com>
2019-09-02 15:57:27 +0700

    jee update

--- wp-content/plugins/woocommerce-privage/privage.php
+++ wp-content/plugins/woocommerce-privage/privage.php
@@ -138,7 +138,6 @@ function privage_login() {
     }
 
     update_user_meta( $current_user->ID, 'token', $access_token );
-    apply_filters('privage_profile', $current_user->ID);
     
   } catch (Exception $e) { 
     echo $e;
